75de9f53256694c7eba0b1b8388e1094848e3a4b
universidad <universidad@10.194.2.2>
2021-10-14 09:44:12 +0100

    Primera Versión Kata03.
    Representación gráfica de un Histograma.

---
+++ src/kata03_42/Kata03_42.java
@@ -0,0 +1,15 @@
+
+package kata03_42;
+
+
+public class Kata03_42 {
+
+   
+    public static void main(String[] args) {
+        
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+        
+    }
+    
+}
